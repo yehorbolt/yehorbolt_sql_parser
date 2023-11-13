e20cdab727ff0a47c8575e63954af2529e758e4a
yehorus <bolotov.yehor@gmail.com>
2023-11-13 18:34:57 +0200

    update 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ extern crate yehorbolt_sql_parser;
 use yehorbolt_sql_parser::parse_sql;
 
 fn main() {
-    let table = "CREATE TABLE financial_report { id INT, currency_name TvEXT, is_usable BOOL}";
+    let table = "CREATE TABLE financial_report 
+    { 
+        id INT, 
+        currency_name TEXT, 
+        is_usable BOOL
+    }";
     let res = parse_sql(table);
     println!("Parsed: {:?}", res);
 }
